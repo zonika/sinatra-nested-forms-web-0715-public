b8fcce3e4c97ce8313817bec00d8f02c75e3a696
zonika <zchodosh@conncoll.edu>
2015-08-26 13:44:00 -0400

    done

--- app.rb
+++ app.rb
@@ -3,7 +3,17 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      "#{params}"
+    end
 
   end
 end
